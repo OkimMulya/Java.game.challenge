4f1b4909170c51a083bf9a8f96fd1cc4def04dc0
OkimMulya <okim.mulya@gmail.com>
2021-10-22 17:13:29 +0700

    game challenge

---
+++ Main.java
@@ -0,0 +1,57 @@
+class Main {
+    public static void main(String[] args) {
+        Hero hero = new Hero("Okim", 25);
+
+        hero.player();
+
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.run();
+    hero.run();
+    hero.eat();
+    hero.eat();
+    hero.eat();
+    hero.drink();
+    hero.drink();
+    hero.jump();
+    hero.jump();
+    hero.jump();
+    hero.jump();
+    hero.jump();
+    hero.jump();
+    hero.sit();
+    hero.sit();
+    hero.sit();
+    hero.sit();
+    hero.sit();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.attack();
+    hero.attack();
+    
+    
+
+
+
+
+
+
+
+    hero.player();
+
+ 
+  }
+}
